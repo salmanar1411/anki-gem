037aa9ce4aff56157ca562faad771570d7f4d0ca
Sam <salmanar644@gmail.com>
2025-04-03 00:33:17 +0700

    connected to public API gemini

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 import express from "express";
+import bodyParser from "body-parser";
+import "dotenv/config"; // dotenv.config() => to read file from file .env
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenAI } from "@google/genai";
 
-const port = 3000;
 const app = express();
+const port = 3000;
+const API_KEY = process.env.API_KEY; // process is a global object that save information about process that is running
+const ai = new GoogleGenAI({ apiKey: process.env.API_KEY });
+
+async function main(prompt) {
+    const response = await ai.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents: `${prompt}`,
+    });
+    return response;
+}
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
+});
+
+app.post("/generate", async (req, res) => {
+    const content = req.body.input;
+   
+    const response = await main(content);
+
+    res.json({message: "in generate now", content: response.text});
 });
 
 app.listen(port, () => {
