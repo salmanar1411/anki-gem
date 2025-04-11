567ccdec8a0ec38c4dff68d2ea8006893a48c0e9
Sam <salmanar644@gmail.com>
2025-04-11 16:47:06 +0700

    add feature can flip all flashcard with easy hard option and add finish button to save the status option to firebase

--- server.js
+++ server.js
@@ -16,68 +16,82 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
-// to read data
-// const isi = await db.collection('users').doc('aturing').get();
-// console.log(isi.data()); => .data() read only the data not additional data
-
-// write data
-// const aTuringRef = db.collection('users').doc('aturing');
-
-// await aTuringRef.set({
-// 'first': 'Alan',
-// 'middle': 'Mathison',
-// 'last': 'Turing',
-// 'born': 1912
-// });
-
 async function generate(prompt) {
-    const response = await ai.models.generateContent({
-      model: "gemini-2.0-flash",
-      contents: `${prompt}`,
-    });
-    return response;
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: `${prompt}`,
+  });
+
+  const text = await response.text();
+  return text;
 }
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+  res.render("index.ejs");
 });
 
 app.post("/generate", async (req, res) => {
-    const content = req.body.input;
-   
-    const response = await generate(content);
+  const content = req.body.input;
+  
+  const response = await generate(content);
 
-    const data =  db.collection("users").doc("materials");
+  const data =  db.collection("users").doc("materials");
 
-    data.set({
-      title: content
-    });
+  data.set({
+    title: content
+  });
 
-    // res.json({message: "in generate now", content: response.text});
-    res.render("generate.ejs", {content: response.text});
+  // res.json({message: "in generate now", content: response.text});
+  res.render("generate.ejs", {content: response.text});
 });
 
 app.post("/generate/create", async (req, res) => {
-    const content = req.body.content;
-    const prompt = content  + "\n Buatkan beberapa flashcard dari materi berikut. Setiap flashcard punya nomor soal (id), pertanyaan (question), jawaban (answer), dan penjelasan (explanation). Formatkan dalam bentuk clean JSON array tanpa markdown dan tanpa formatting";
-    const response = await generate(prompt);
-    const id = 0;
-    
-    let raw = response.text
-    raw = raw.replace(/```json|```/g, '');
-    raw = raw.trim();
-    const flashcard = JSON.parse(raw);
-    // res.json({output: file});
-    // const flashcard = JSON.stringify(response.text);
+  const content = req.body.content;
+  const prompt = content  + "\n Buatkan beberapa flashcard dari materi berikut. Setiap flashcard punya pertanyaan (question), jawaban (answer), dan penjelasan (explanation). Formatkan dalam bentuk clean JSON array tanpa markdown dan tanpa formatting";
+  const response = await generate(prompt);
+  
+  let raw = response;
+  raw = raw.replace(/```json|```/g, '').trim();
+  const flashcard = JSON.parse(raw);
+  // res.json({output: file});
+  // const flashcard = JSON.stringify(response.text);
 
-    const data =  db.collection("users").doc("materials");
+  const data = db.collection("users").doc("materials");
 
-    data.set({
-      materials: req.body.content,
-      flashcard: {flashcard}
-    });
+  data.set({
+    materials: req.body.content,
+    flashcard: {flashcard}
+  });
+
+  // why we need to use await
+  const isi = await db.collection("users").doc("materials").get(); 
+  // const data = isi.docs.map(doc => doc.data());
+  const card = isi.data().flashcard.flashcard;
+  console.log(card[0]);
+  res.render("flashcard.ejs", {flashcard: card})
+});
+
+app.post("/generate/finish", async (req, res) => {
+  const value = req.body.flashcardValue;
+
+  const docRef = db.collection("users").doc("materials"); 
+  const get = await docRef.get();
+  const card = get.data().flashcard.flashcard;
+
+  value.forEach((element, index) => {
+    card[index].status = element;
+  });
+
+  await docRef.set({
+    flashcard: {
+      flashcard: card
+    }
+  });
+
+  res.json({message: "update success"});
 });
 
 app.listen(port, () => {
